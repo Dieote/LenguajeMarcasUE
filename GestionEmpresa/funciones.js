babfd2f3f7b4909c4570abe9602d84bd5e97b89e
Dieote <gomezmdiego97@gmail.com>
2025-03-08 17:37:36 +0100

    Eliminar
    Creacion de input de eliminar con id mas estilos y Modal funcionando.

--- GestionEmpresa/funciones.js
+++ GestionEmpresa/funciones.js
@@ -18,3 +18,37 @@ function accionesForm() {
             accionesDiv.style.display = "none"; // Oculta el campo si no es Jefe
         }
 }
+
+function cargarFormEliminar() {
+    fetch('formEliminar.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('mostrarFormEliminar').innerHTML = data;
+            document.getElementById('mostrarFormEliminar').style.display = 'block';
+        })
+        .catch(error => console.error('Error cargando la seccion de Eliminar:', error));
+}
+
+//Confirmacion de eliminacion
+function openModal() {
+    const idTrabajador = document.getElementById("id").value.trim();
+    if (idTrabajador !== "") {
+        document.getElementById('modalEliminar').style.display = 'flex';
+    }
+    else {
+        alert("Debe ingresar el ID del trabajador a eliminar.");
+        }
+}
+function closeModal() {
+    document.getElementById('modalEliminar').style.display = 'none';
+}
+
+function confirmarEliminacion() {
+    const idTrabajador = document.getElementById("id").value.trim();
+    if (idTrabajador !== "") {
+        alert(`Trabajador con ID ${idTrabajador} eliminado.`);
+        closeModal(); // Cierra el modal después de confirmar
+    } else {
+        alert("Error: ID no válido.");
+    }
+}
