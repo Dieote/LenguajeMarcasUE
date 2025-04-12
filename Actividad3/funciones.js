bc17a9beb91c74426bf98f36d66bc171deaa9580
Dieote <gomezmdiego97@gmail.com>
2025-04-12 12:50:02 +0200

    Funcionalidad teclado
    Correcciones en  la funcionalidad del teclado

--- Actividad3/funciones.js
+++ Actividad3/funciones.js
@@ -1,4 +1,5 @@
 let mayusculaActiva = false;
+let inputActivo = null;
 
 function openTeclado() {
     document.getElementById('cajaTeclado').style.display = 'flex';
@@ -8,42 +9,53 @@ function closeTeclado() {
     document.getElementById('cajaTeclado').style.display = 'none';
 }
 
-/*document.addEventListener("DOMContentLoaded", function () {
-    const input = document.getElementById("inputEscritura");
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll("input").forEach(input => {
+        input.addEventListener("focus", () => {
+            inputActivo = input;
+            openTeclado();
+        });
+    });
 
     // Letras
     document.querySelectorAll(".letra").forEach(btn => {
         btn.addEventListener("click", () => {
-            let letra = btn.textContent;
-            input.value += mayusculaActiva ? letra.toUpperCase() : letra;
+            if (inputActivo){
+                let letra = btn.textContent;
+                inputActivo.value += mayusculaActiva ? letra.toUpperCase() : letra;
+            }
         });
     });
 
     // Números y símbolos
     document.querySelectorAll(".numero, .simbolo").forEach(btn => {
         btn.addEventListener("click", () => {
-            input.value += btn.textContent;
+            if (inputActivo){
+                inputActivo.value += btn.textContent;
+            }
         });
     });
 
     // Borrar
     document.getElementById("borrar").addEventListener("click", () => {
-        input.value = input.value.slice(0, -1);
+        if(inputActivo){
+            inputActivo.value = inputActivo.value.slice(0, -1);
+        }
     });
 
     // Espacio
     document.getElementById("espacio").addEventListener("click", () => {
-        input.value += " ";
+        inputActivo.value += " ";
     });
 
     // Enter
     document.getElementById("enter").addEventListener("click", () => {
-        input.value += "\n"; // o podrías enviar un form, según tu caso
+        inputActivo.value += "\n"; // o podrías enviar un form, según tu caso
     });
 
     // Mayúsculas
     document.getElementById("mayus").addEventListener("click", () => {
         mayusculaActiva = !mayusculaActiva;
-        document.getElementById("mayus").style.backgroundColor = mayusculaActiva ? "#ccc" : "";
+        document.getElementById("mayus").style.backgroundColor = mayusculaActiva ? "#db43a9" : "";
     });
-});*/
+});
